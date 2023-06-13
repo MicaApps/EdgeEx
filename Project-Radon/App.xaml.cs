402e68817621d3111db66124b11032da15723108
Denissssse <Shomnipotence@hotmail.com>
2023-06-13 09:19:50 +0800

    Remove Welcome Page

--- Project-Radon/App.xaml.cs
+++ Project-Radon/App.xaml.cs
@@ -65,7 +65,7 @@ namespace Yttrium_browser
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(oobe1), e.Arguments);
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
