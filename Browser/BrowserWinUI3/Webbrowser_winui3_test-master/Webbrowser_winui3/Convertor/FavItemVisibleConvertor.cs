4762f74b16c3dca8cc3eed75ac8c888638c70bd6
潇阳 邹 <wanna_zxy@yeah.net>
2023-07-03 00:22:17 +0800

    add valueconvertor to return Visibility.Collapsed when input collection is empty

---
+++ Browser/BrowserWinUI3/Webbrowser_winui3_test-master/Webbrowser_winui3/Convertor/FavItemVisibleConvertor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webbrowser_winui3.Convertor
+{
+   public  class FavItemVisibleConvertor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var data=value as IEnumerable<object>;
+            if (data.Any())
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
